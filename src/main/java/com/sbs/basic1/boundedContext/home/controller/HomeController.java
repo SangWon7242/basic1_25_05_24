a8282f4fef8850764f2d3e084c0b4a8d40c49547
SangWon7242 <gotkdqja@gmail.com>
2025-05-24 14:45:17 +0900

    HomeController 도입

---
+++ src/main/java/com/sbs/basic1/boundedContext/home/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.sbs.basic1.boundedContext.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+// '개발자가 스프링부트한테 해당 클래스는 컨트롤러 클래스이다.'라고 전달
+public class HomeController {
+  @GetMapping("/home/main")
+  // 개발자가 스프링부트한테
+  // 만약에 '/home/main'이라는 요청이 들어오면
+  // 아래 메서드를 실행시켜줘!
+  @ResponseBody
+  // 개발자가 스프링부트한테
+  // 이 메서드가 실행된 결과를
+  // Body의 응답으로 보내줘!
+  public String showHomeMain() {
+    return "반갑습니다.";
+  }
+}
