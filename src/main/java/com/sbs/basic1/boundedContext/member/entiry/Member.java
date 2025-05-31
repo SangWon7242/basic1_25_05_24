7eb4fe09df7a7c24762d4c5236d67a15960d4b95
SangWon7242 <gotkdqja@gmail.com>
2025-05-31 13:31:15 +0900

    MemberService, MemberRepository 도입

---
+++ src/main/java/com/sbs/basic1/boundedContext/member/entiry/Member.java
@@ -0,0 +1,21 @@
+package com.sbs.basic1.boundedContext.member.entiry;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Member {
+  private static long lastId;
+  private final long id;
+  private final String username;
+  private String password;
+
+  static {
+    lastId = 0;
+  }
+
+  public Member(String username, String password) {
+    this(++lastId, username, password);
+  }
+}
--- src/main/java/com/sbs/basic1/boundedContext/member/service/MemberService.java
+++ src/main/java/com/sbs/basic1/boundedContext/member/service/MemberService.java
@@ -1,18 +1,29 @@
 package com.sbs.basic1.boundedContext.member.service;
 
 import com.sbs.basic1.base.rsData.RsData;
+import com.sbs.basic1.boundedContext.member.entiry.Member;
+import com.sbs.basic1.boundedContext.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
+  private MemberRepository memberRepository;
+
+  public MemberService() {
+    memberRepository = new MemberRepository();
+  }
+
   public RsData tryLogin(String username, String password) {
-    if(!username.equals("user1")) {
-      return RsData.of("F-1", "%s(은)는 존재하지 않는 회원입니다.".formatted(username));
-    }
-    else if(!password.equals("1234")) {
-      return RsData.of("F-2", "비밀번호가 일치하지 않습니다.");
+    Member member = memberRepository.findByUsername(username);
+
+    if(member == null) {
+      return RsData.of("F-3", "%s(은)는 존재하지 않는 회원입니다.".formatted(username));
     }
 
-    return RsData.of("S-1", "%s님 환영합니다.\n".formatted(username));
+    if(!member.getPassword().equals(password)) {
+      return RsData.of("F-4", "비밀번호가 일치하지 않습니다.");
+    }
+
+    return RsData.of("S-1", "%s님 환영합니다.".formatted(username));
   }
 }
